e0b78645881606cdec0147b985339b048ab4385d
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2022-12-01 17:03:12 +0100

    wieso wird der context nicht auf local sotrage gespeichert

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,4 +1,8 @@
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Friend } from 'src/app/models/Friend';
+import { User } from 'src/app/models/User';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
     selector: 'app-friends',
@@ -6,8 +10,25 @@ import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
     styleUrls: ['../../../assets/css/components.css']
 })
 export class FriendsComponent implements OnInit {
+    public user: User = new User();
+    public freinds: Friend[] = [];
 
-    public constructor() {
+    public constructor(private router: Router, private service: BackendService) {
+        this.service.loadCurrentUser()
+        .subscribe((res: User | null) => {
+            if(res != null) {
+                this.user = res;
+                console.log(this.user);
+            }
+            else {
+                //this.router.navigate(["/"]);
+            }
+        })
+
+    this.service.loadFriends()
+        .subscribe((res: Array<Friend>) => {
+            console.log(res);
+        })
     }
 
     public ngOnInit(): void {
