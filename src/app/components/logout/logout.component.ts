f004a269b0c4f33fd855c99acb10b092863d7881
MysterionAutotronic <76444188+MysterionAutotronic@users.noreply.github.com>
2022-12-01 17:15:03 +0100

    finished logout

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
   }
+
+  public login(): void {
+    this.changeRoute('/login');      
+  }
+
+  public changeRoute(route: string): void {
+    this.router.navigate([route])
+}
 
 }
