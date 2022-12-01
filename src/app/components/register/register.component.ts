5a505ce24d6478aa71415e8285564b56d28e9b84
MysterionAutotronic <76444188+MysterionAutotronic@users.noreply.github.com>
2022-12-01 01:35:43 +0100

    started working on register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit, Input, Directive } from '@angular/core';
+import { NG_VALIDATORS, AbstractControl, Validator, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
-    styleUrls: ['../../../assets/css/components.css']
+    styleUrls: ['../../../assets/css/components.css'],
 })
+
 export class RegisterComponent implements OnInit {
+
+    password1 = '';
+    password2 = '';
+    username = '';
 
     public constructor() { 
     }
@@ -13,4 +19,41 @@ export class RegisterComponent implements OnInit {
     public ngOnInit(): void {
     }
 
+}
+
+@Directive({
+    selector: '[appForbiddenName]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: ForbiddenValidatorDirective,
+        multi: true
+    }]
+})
+
+export class ForbiddenValidatorDirective implements Validator {
+
+    @Input('appForbiddenName') forbiddenName = '';
+
+    validate(control: AbstractControl): ValidationErrors | null {
+        return this.forbiddenName ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
+                                  : null;
+      }
+}
+
+@Directive({
+    selector: '[appMatch]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: MatchValidatorDirective,
+        multi: true
+    }]
+})
+
+export class MatchValidatorDirective implements Validator {
+
+    @Input('appMatch') match = '';
+
+    validate(control: AbstractControl): ValidationErrors | null {
+        return;
+    }
 }
